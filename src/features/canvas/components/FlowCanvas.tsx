df8366d14d256e5457c65e6cf430ad0d8de0ac68
ipon1207 <ipon_1207@yahoo.co.jp>
2026-02-16 22:05:36 +0900

    feat: コンポーネントの実装

---
+++ src/features/canvas/components/FlowCanvas.tsx
@@ -0,0 +1,29 @@
+import {
+	Background,
+	BackgroundVariant,
+	Controls,
+	ReactFlow,
+} from "@xyflow/react";
+import { useCanvasStore } from "../stores/useCanvasStore";
+
+export function FlowCanvas() {
+	// Storev から必要なデータと関数を取り出す
+	const { nodes, edges, onNodesChange, onEdgesChange, onConnect } =
+		useCanvasStore();
+
+	return (
+		<div className="h-full w-full">
+			<ReactFlow
+				nodes={nodes}
+				edges={edges}
+				onNodesChange={onNodesChange}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
+				fitView
+			>
+				<Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+				<Controls />
+			</ReactFlow>
+		</div>
+	);
+}
