b7fc7c869c6168f67548dbd5a3c06d00d3f45215
ipon1207 <ipon_1207@yahoo.co.jp>
2026-02-16 21:59:07 +0900

    feat: Store の実装

---
+++ src/features/canvas/stores/useCanvasStore.ts
@@ -0,0 +1,59 @@
+import {
+	addEdge,
+	applyEdgeChanges,
+	applyNodeChanges,
+	type Connection,
+	type EdgeChange,
+	type NodeChange,
+} from "@xyflow/react";
+import { create } from "zustand";
+import type { AppEdge, AppNode } from "../types/canvas";
+
+// StateとActionの型定義
+type CanvasState = {
+	nodes: AppNode[];
+	edges: AppEdge[];
+
+	// Actions
+	onNodesChange: (changes: NodeChange[]) => void;
+	onEdgesChange: (changes: EdgeChange[]) => void;
+	onConnect: (connection: Connection) => void;
+	setNodes: (nodes: AppNode[]) => void;
+};
+
+export const useCanvasStore = create<CanvasState>((set, get) => ({
+	nodes: [
+		// 初期データのサンプル
+		{
+			id: "1",
+			position: { x: 100, y: 100 },
+			data: { label: "Start Learning" },
+			type: "default",
+		},
+	],
+	edges: [],
+
+	// ノードがドラッグ等で変更されたときに呼ばれる
+	onNodesChange: (changes) => {
+		set({
+			nodes: applyNodeChanges(changes, get().nodes) as AppNode[],
+		});
+	},
+
+	// エッジが変更されたときに呼ばれる
+	onEdgesChange: (changes) => {
+		set({
+			edges: applyEdgeChanges(changes, get().edges) as AppEdge[],
+		});
+	},
+
+	// 線が接続されたときに呼ばれる
+	onConnect: (connection) => {
+		set({
+			edges: addEdge(connection, get().edges) as AppEdge[],
+		});
+	},
+
+	// データを丸ごとセットする場合（ロード機能用）
+	setNodes: (nodes) => set({ nodes }),
+}));
