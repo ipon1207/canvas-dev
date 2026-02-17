817801c40faa189791d059d63083271699900f49
ipon1207 <ipon_1207@yahoo.co.jp>
2026-02-17 12:50:32 +0900

    feat: ツールバーコンポーネントの作成

---
+++ src/features/canvas/components/CanvasToolbar.tsx
@@ -0,0 +1,19 @@
+import { Panel } from "@xyflow/react";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useCanvasStore } from "../stores/useCanvasStore";
+
+export function CanvasToolbar() {
+	const { addNode } = useCanvasStore();
+
+	return (
+		<Panel position="top-right" className="m-4">
+			<Button
+				onClick={addNode}
+				className="shadow-lg hover:scale-105 transition-transform"
+			>
+				<Plus className="mr-2 h-4 w-4">Add Node</Plus>
+			</Button>
+		</Panel>
+	);
+}
